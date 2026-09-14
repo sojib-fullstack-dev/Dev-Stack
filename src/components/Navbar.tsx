e808e77a06a0e819c6a576ec8c69d9ba985235b1
samiulhasansojibe <samiulhasansojib069@gmail.comm>
2026-09-14 11:42:07 +0600

    resolved nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import myImg from "../assets/logo-text.png";
 const Navbar = () => {
   return (
     <nav className=" container mx-auto  border-b sticky top-0  z-50 border-gray-100">
-      <div className="flex justify-between items-center   py-5 ">
+      <div className="flex justify-between items-center   py-5 bg-white ">
         <span className="block md:hidden text-3xl px-5"><FiAlignJustify /></span>
         <img src={myImg} alt="" />
         <ul className=" hidden md:flex  gap-5 items-center">
